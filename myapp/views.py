3e6525cf7439bfaaf8faed3bdabf6348cf3796d8
prince <princekirad11@gmail.com>
2023-09-14 08:45:22 +0530

    create template & static content and working on frontend form validation

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def Home(request):
+    return render(request, "home.html")
+
+def Opportunities(request):
+    return render(request, "opportunities.html")
